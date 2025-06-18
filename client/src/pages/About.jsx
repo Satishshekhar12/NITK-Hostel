d8d43bcad8788d14f9d51a649eb54b4cf869a89d
Vanshita Tripathi <vanshupro99@gmail.com>
2025-06-18 10:10:12 +0530

    ITs not the final

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+// import React from 'react'
 
-function About() {
-    return (
-        <div>
-            <h1>About</h1>
-        </div>
-    )
-}
+// function About() {
+//     return (
+//         <div>
+//             <h1>About</h1>
+//         </div>
+//     )
+// }
+
+// export default About;
+// src/pages/About.jsx
+import React from 'react';
+import StaffSection from "./StaffSection"; // ✅ Make sure path is correct
+import styles from '../styles/about.module.css'; // ✅ Import as a module
+
+const About = () => {
+  return (
+    <div className={styles['about-bg-slideshow']}>
+      <div className={styles['about-bg-slide'] + ' ' + styles['about-bg1']}></div>
+      <div className={styles['about-bg-slide'] + ' ' + styles['about-bg2']}></div>
+      <div className={styles['about-container']}>
+        <h1 className={styles['about-heading']}>About Us</h1>
+        {/* <p className={styles['about-description']}>
+        </p> */}
+        <StaffSection />
+      </div>
+    </div>
+  );
+};
 
 export default About;
