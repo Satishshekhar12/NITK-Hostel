0679bfa0d7d598b26feb51e472f1f68084155e41
Nagender <nagenderrajput54@gmail.com>
2025-06-21 18:42:24 +0530

    admin login router and styles changes

--- client/src/pages/AdminLogin.jsx
+++ client/src/pages/AdminLogin.jsx
@@ -3,21 +3,21 @@ import styles from '../styles/adminlogin.module.css';
 
 const AdminLogin = () => {
     return (
-        <div className={styles.container}>
-            <h2> Admin Login</h2>
-            <form className={styles.form}>
-                <label>
-                    Username: 
-                    <input type='text' placeholder='Enter UserName'/>
-                </label>
-                <label>
-                    Password:
-                    <input type='password' placeholder='Enter Password'/>
-                </label>
-                <button type='submit'>Login</button>
-
-            </form>
-
+        <div className={styles.background}>
+            <div className={styles.container}>
+                <h2> Admin Login</h2>
+                <form className={styles.form}>
+                    <label>
+                        Username: 
+                        <input type='text' placeholder='Enter UserName'/>
+                    </label>
+                    <label>
+                        Password:
+                        <input type='password' placeholder='Enter Password'/>
+                    </label>
+                    <button type='submit'>Login</button>
+                </form>
+            </div>
         </div>
     );
 };
